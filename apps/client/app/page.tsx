97137f48b11a8291d41395f0664b0a3fa9601b5d
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-04-12 01:11:26 +0530

    Project Setup Started

---
+++ apps/client/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../components/ui/button";
+
+export default function Page(): JSX.Element {
+  return (
+    <>
+      <div className="">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
